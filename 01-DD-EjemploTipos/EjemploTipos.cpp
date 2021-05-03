527ca23663bd9bb93c18ff3ef57a2fedf9ddecc5
mariacv24 <82426487+mariacv24@users.noreply.github.com>
2021-05-03 20:58:41 -0300

    Add files via upload

---
+++ 01-DD-EjemploTipos/EjemploTipos.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<cassert>
+using namespace std;
+int main () {
+
+assert(true==true) ; // bool
+char letra = 'o'; //char
+assert(letra=='o');
+unsigned i=2; //unsigned
+assert(i==2);
+int x=1 , y=2; //int
+assert(3==x+y);
+double z=10 , w=7; //double
+assert(7/10==7/10);
+string nombre = "Maria"; //string
+assert(nombre=="Maria");
+}
+
---
+++ 01-DD-EjemploTipos/readme.md.cpp
@@ -0,0 +1,18 @@
+# Algoritmos y Estructuras de Datos
+
+-Curso: K1051
+
+-Año : 2021
+
+-Legajo: 171092-8
+
+-Apellidos: Cabello Vergara
+
+-Nombre: Maria de los Angeles
+
+-IDE : Dev C++
+
+-Compilador: ISO C++11
+
+-Credito Extra: ¿ Son realmente todos los tipos de datos que vimos en clase?
+-Los tipos de datos que utilizamos para hacer este tp#1 fueron los que estaban en los slides de la clase, sin embargo hay otros tipos de datos como float,long double,long int, short int.
